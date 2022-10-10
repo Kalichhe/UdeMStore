21ff1f3722d4b70442a6827e91401179e94f82a7
Kalichhe <jhondh2003@gmail.com>
2022-10-10 14:30:54 -0500

    Ya todo por hoy

--- vistas/home.php
+++ vistas/home.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="./css/estilos.css">
+<!-- <link rel="stylesheet" href="./css/estilos.css"> -->
 	<div class="container is-fluid">
 		<h1 class="title">Principal</h1>
 		<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
